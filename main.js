8f96e0dbb9e32270cc6369e1d779eb44cf17acb4
Weenty <73786123+Weenty@users.noreply.github.com>
2022-11-11 11:20:40 +0500

    fd

--- main.js
+++ main.js
@@ -26,7 +26,6 @@ document.addEventListener('DOMContentLoaded', function(){
                 if (result==='granted') {
                     Notification(imt)
                 }   
-            })
         }, 10000);
        })
     })
